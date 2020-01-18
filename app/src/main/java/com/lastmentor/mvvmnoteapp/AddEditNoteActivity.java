6897c383cf98903556d4b8596e09a3cac49e51da
Rıza Özonuk <riza.gs1905@hotmail.com>
2020-01-18 16:29:54 +0300

    Project Added

---
+++ app/src/main/java/com/lastmentor/mvvmnoteapp/AddEditNoteActivity.java
@@ -0,0 +1,96 @@
+package com.lastmentor.mvvmnoteapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddEditNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_ID =
+            "com.codinginflow.architectureexample.EXTRA_ID";
+    public static final String EXTRA_TITLE =
+            "com.codinginflow.architectureexample.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.codinginflow.architectureexample.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY =
+            "com.codinginflow.architectureexample.EXTRA_PRIORITY";
+
+    private EditText txtTitle,txtDesc;
+    private NumberPicker nmbPicker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDesc = findViewById(R.id.txtDesc);
+        nmbPicker = findViewById(R.id.nmbPicker);
+
+        nmbPicker.setMinValue(1);
+        nmbPicker.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra(EXTRA_ID)){
+            setTitle("Edit Note");
+            txtTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+            txtDesc.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            nmbPicker.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+        }else {
+            setTitle("Add Note");
+        }
+    }
+
+    private void saveNote(){
+        String title = txtTitle.getText().toString();
+        String description = txtDesc.getText().toString();
+        int priority = nmbPicker.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if(id != -1){
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
